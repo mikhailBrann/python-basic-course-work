18cd2a387ef5090f1e71ff183b689c40e81c7e0d
mikhailBrann <alunos13@yandex.ru>
2021-10-14 23:05:02 +0300

    change vk methods

--- lib_class.py
+++ lib_class.py
@@ -19,34 +19,20 @@ class VkInfo:
 
     def get_vk_info(self, user_id):
         # https://api.vk.com/method/users.get?user_ids=210700286&fields=bdate&access_token=533bacf01e11f55b536a565b57531ac114461ae8736d6506a3&v=5.131
-        
-        # params = {'owner_id': 1, 'access_token': self.token, 'v': '5.131', 'album_id': 'profile'}
         request_info = False
-        
-        # если id передали как число
-        if type(user_id) == int or user_id.isdigit():
-            method = 'photos.get'
-            params = {'owner_id': user_id, 'v': '5.131', 'album_id': 'profile'}
-            responce = self.get_vk_request(method, params)
-            
-            if responce:
-                request_info = responce
-        
-        # если id передали как псевдоним    
-        elif user_id.isdigit() is False:
+
+        # если id передали как псевдоним 
+        if str(user_id).isdigit() is False:
             # получаем id пользователя
             get_user_id = self.get_vk_request('users.get', {'user_ids': user_id, 'v': '5.131'})
             user_id = get_user_id['response'][0]['id']
-            
-            method = 'photos.get'
-            params = {'owner_id': user_id, 'v': '5.131', 'album_id': 'profile'}
-            responce = self.get_vk_request(method, params)
-            
-            if responce:
-                request_info = responce
-            
-    
         
-            
+        get_photo_params = {'owner_id': user_id, 'v': '5.131', 'album_id': 'profile', 'extended': 'likes', 'photo_sizes': '1'}
+        get_photo_method = 'photos.get'
+        
+        responce = self.get_vk_request(get_photo_method, get_photo_params)
+        if responce:
+            request_info = responce
+        
         return request_info
 
--- main.py
+++ main.py
@@ -50,6 +50,8 @@ from lib_class import VkInfo
 
 
 if __name__ == '__main__':
-    vk_init = VkInfo('958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008')
+    vk_init = VkInfo(...)
     user_vk = vk_init.get_vk_info('begemot_korovin')
-    pprint(user_vk)
+    
+    if user_vk:
+        pprint(user_vk)
